7523b7718ed89cd1d2879311557383207cf3521d
Hana Takatori <hanatakatori@MacBook-Pro-10.local>
2026-06-18 08:59:45 +0900

    opt_2 done

--- week5/solver_greedy copy.py
+++ week5/solver_greedy copy.py
@@ -3,13 +3,11 @@
 import sys
 
 from common import print_tour, read_input
+import math
 
 
 def solve(cities):
-    # list of (x,y) coordinates
-    # Build a trivial solution.
-    # Visit the cities in the order they appear in the input.
-    return greedy(cities)
+    return opt_2(greedy(cities), cities)
 
 def greedy(cities):
     """
@@ -18,6 +16,7 @@ def greedy(cities):
     - From current position, scan all unvisited points and pick the closest
     - Move there, mark visited, repeat
     - Stop when all points are visited
+    - returning the list of index in order of visit
     """
     #start from the city at the very corner(smallest)
     start_ind = find_corner(cities)
@@ -33,6 +32,34 @@ def greedy(cities):
         #update the current city to next (here it is an actua coordinate not an index)
         cur_city = cities[next_city]
     return path
+
+def opt_2(path, cities):
+    """
+    take any two edges that doesn't share a node
+    -> check if swapping those  two edges make the path shorter
+    -> repeat this for all possible combinations
+    will use on-spot swapping instead of finding the local optima
+    continue the swap until there's no swap happen
+    """
+    #keep track of if there was any swap happened
+    swaped = True
+    while swaped:
+        swaped = False
+        for i in range(len(path)):
+            #make sure there's no overlap between two edges
+            for j in range(i+2, len(path)):
+                #specific case to keep in mind where it shares cities[0]
+                if i == 0 and j == len(path)-1:
+                    continue
+                pre_dist = calculate_distance(cities[path[i]], cities[path[(i+1)%len(path)]]) + calculate_distance(cities[path[j]], cities[path[(j+1)%len(path)]])
+                #calculate the distance when i and j is swapped
+                post_dist = calculate_distance(cities[path[i]], cities[path[j]]) + calculate_distance(cities[path[(i+1)%len(path)]], cities[path[(j+1)%len(path)]])
+                if pre_dist > post_dist:
+                    #reverse the segment in between to connect i and j, i+1 and j+1
+                    path[i+1:j+1] = path[i+1:j+1][::-1]
+                    swaped = True
+    return path
+
 
 def find_corner(cities):
     """
@@ -62,7 +89,7 @@ def calculate_distance(start, end):
     """
     x1, y1 = start
     x2, y2 = end
-    return (x2-x1)**2 + (y2-y1)**2
+    return math.sqrt((x2-x1)**2 + (y2-y1)**2)
 
 def get_closest(current, cities, visited):
     """return index of the new city for convinience"""
